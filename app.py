dc0b33ae369dec1303d35a560f8de75ec9be65b2
aa-tolmachev <taa.tolmachev@gmail.com>
2017-08-08 22:22:59 +0500

    TEST

--- app.py
+++ app.py
@@ -18,21 +18,21 @@ def hello():
 
 
 
-#@application.route('/family_budget', methods=['GET', 'POST'])
-#def app_fb():
-#    try:
-#        #json_params = json.loads(request.get_data())
-#
-#        family_budget.main()
-#        
-#        return 'END'
-#    except:
-#
-#        return 'ERROR'
+@application.route('/family_budget', methods=['GET', 'POST'])
+def app_fb():
+    try:
+        #json_params = json.loads(request.get_data())
+
+        family_budget.main()
+        
+        return 'END'
+    except:
+
+        return 'ERROR'
 
 
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
     application.run(debug=False, port=port, host='0.0.0.0')
-#    application.run()
+
