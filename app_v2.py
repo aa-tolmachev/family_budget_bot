26b58efc7b3ebccb6c1ae53d7b09b98d1899388f
aa-tolmachev <taa.tolmachev@gmail.com>
2017-08-12 21:11:15 +0500

    3

--- app_v2.py
+++ app_v2.py
@@ -1,33 +1,22 @@
 import telebot
-import os
-from flask import Flask, request
-
 import access
 
 token = access.token()
+api = access.api()
 
 bot = telebot.TeleBot(token)
-server = Flask(__name__)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
 
 
-@server.route("/bot", methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://fam-budg-bot.herokuapp.com/bot")
-    return "!", 200
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
 
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+
+
+bot.polling()
