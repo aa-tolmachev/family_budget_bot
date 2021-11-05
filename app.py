9380c48dcdbb750923edaed8016b56b46816ecbc
Aleksandr Tolmachev <a.tolmachev@xsolla.com>
2021-11-05 16:21:28 +0500

    heroku update 3

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ def hello():
     return "Hello World!"
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.getenv('PORT', 5000))
+    application.run(debug=False, port=port, host='0.0.0.0' , threaded=True)
