39aa4e2e459171036eb5f0f12d7f0f9f44692117
Aleksandr Tolmachev <a.tolmachev@xsolla.com>
2021-11-05 16:09:48 +0500

    heroku update 3

--- app.py
+++ app.py
@@ -207,8 +207,8 @@ def main():
 
 
 
-#if __name__ == "__main__":
-#    port = int(os.getenv('PORT', 5000))
-#    application.run(debug=False, port=port, host='0.0.0.0' , threaded=True)
+if __name__ == "__main__":
+    port = int(os.getenv('PORT', 5000))
+    application.run(debug=False, port=port, host='0.0.0.0' , threaded=True)
 
 
