956ed22f38842a3d1160ca65ac202e616fbd8b07
Aleksandr Tolmachev <aletolmachev@ozon.ru>
2022-11-23 14:49:02 +0300

    update main menu

--- dicts/meta_info.py
+++ dicts/meta_info.py
@@ -1,4 +1,4 @@
-reply_markup_main = {'keyboard': [['Отчеты','Кошелек'],['Инвестиции','Дела']], 'resize_keyboard': True, 'one_time_keyboard': False}
+reply_markup_main = {'keyboard': [['Отчеты','Кошелек'],['Дела']], 'resize_keyboard': True, 'one_time_keyboard': False}
 
 
 markup_non_important_to_tommorow = {'keyboard': [['Перенеси неважные на завтра'], ['меню']],'resize_keyboard': True,'one_time_keyboard': False}
